42cb5eaea65cb3c3b2c2136dc60cf0e2f78ec917
Octowl <alsaff1987@gmail.com>
2024-10-20 11:06:20 +0300

    fetch books and find

--- src/app/books/[id]/page.js
+++ src/app/books/[id]/page.js
@@ -1,9 +1,15 @@
 import BookCard from "@/components/BookCard";
-import books from "@/data/books";
 
 async function BookDetailPage({ params }) {
   const { id } = await params;
+
+  const response = await fetch(
+    "https://coded-books-api-crud.eapi.joincoded.com/books"
+  );
+  const books = await response.json();
+
   const book = books.find((book) => book.id === +id);
+
   return <BookCard book={book} />;
 }
 
