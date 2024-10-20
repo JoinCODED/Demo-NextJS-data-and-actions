76b7567506379c6456c9cd80c6a3c4644ae50aa3
Octowl <alsaff1987@gmail.com>
2024-10-20 12:26:53 +0300

    move fetching to actions

--- src/app/books/[id]/page.js
+++ src/app/books/[id]/page.js
@@ -1,13 +1,9 @@
+import { fetchBooks } from "@/actions";
 import BookCard from "@/components/BookCard";
 
 async function BookDetailPage({ params }) {
   const { id } = await params;
-
-  const response = await fetch(
-    "https://coded-books-api-crud.eapi.joincoded.com/books"
-  );
-  const books = await response.json();
-
+  const books = await fetchBooks();
   const book = books.find((book) => book.id === +id);
 
   return <BookCard book={book} />;
