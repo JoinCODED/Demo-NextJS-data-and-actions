21f7e798f7e25855f5b94d620efd1782bf16a1a4
Octowl <alsaff1987@gmail.com>
2024-10-20 12:48:39 +0300

    book form

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,9 +1,15 @@
 import { fetchBooks } from '@/actions'
+import BookForm from '@/components/BookForm'
 import BookList from '@/components/BookList'
 
 async function BooksPage() {
   const books = await fetchBooks()
-  return <BookList books={books} />
+  return (
+    <>
+      <BookList books={books} />
+      <BookForm />
+    </>
+  )
 }
 
 export default BooksPage
---
+++ src/components/BookForm.js
@@ -0,0 +1,50 @@
+'use client'
+
+import { useState } from "react"
+
+function BookForm() {
+  const [book, setBook] = useState({
+    title: "",
+    price: 0,
+    description: ""
+  })
+
+  function handleChange(e) {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    // DO SOMETHING WITH THE DATA
+    console.log(book)
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-3 my-10 text-black">
+      <input
+        name="title"
+        value={book.title}
+        onChange={handleChange}
+        placeholder="title" />
+      <input
+        type="number"
+        name="price"
+        value={book.price}
+        onChange={handleChange}
+        placeholder="price" />
+      <textarea
+        name="description"
+        value={book.description}
+        onChange={handleChange}
+        placeholder="description" />
+      <button type="submit">MAKE A BOOK</button>
+    </form>
+  )
+}
+
+export default BookForm
