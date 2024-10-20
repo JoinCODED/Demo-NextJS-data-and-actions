fd0b05e36ca7bd05231a31696ff2d10b2213f071
Octowl <alsaff1987@gmail.com>
2024-10-20 09:37:48 +0300

    shrink data

--- src/data/books.js
+++ src/data/books.js
@@ -7,13 +7,6 @@ const books = [
     "title": "Design by Apple California"
   },
   {
-    "title": "A Song of Ice and Fire part 2",
-    "price": 134.5,
-    "image": "https://upload.wikimedia.org/wikipedia/en/d/dc/A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg",
-    "description": "A Song of Ice and Fire is a series of epic fantasy novels by the American novelist and screenwriter George R. R. Martin. He began the first volume of the series, A Game of Thrones, in 1991, and it was published in 1996.",
-    "id": 7
-  },
-  {
     "title": "Rich poor Lord",
     "price": 1.5,
     "image": "https://upload.wikimedia.org/wikipedia/en/b/b9/Rich_Dad_Poor_Dad.jpg",
